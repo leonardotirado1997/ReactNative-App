ffcda8bb394b057d02ed7172a0cf97aabef95d7d
Leonardo Pires Tirado <leonardo.tirado@docente.pr.senac.br>
2025-09-01 14:28:44 -0300

    criando style dentro de components e comecando a componentizaco do codigo

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,20 +1,38 @@
-import React, { forwardRef } from "react";
-import { View, Text, TextInput } from 'react-native';
+import React, { forwardRef, Fragment } from "react";
+import { View, Text, TextInput, TextInputProps } from 'react-native';
 import { style } from "../../pages/login/styles"; // Gamb
-import { MaterialIcons } from '@expo/vector-icons';
+// import { MaterialIcons } from '@expo/vector-icons'; -- Vou Tirar esse dps!
 import { themas } from "../../global/themes";
+import { FontAwesome, MaterialIcons, Octicons } from '@expo/vector-icons';
+
+type IconComponent = React.ComponentType<React.ComponentProps<typeof MaterialIcons>> |
+    React.ComponentType<React.ComponentProps<typeof FontAwesome>> |
+    React.ComponentType<React.ComponentProps<typeof Octicons>>;
+
+type Props = TextInputProps & {
+    IconLeft?: IconComponent, // Icones Nativos
+    IconRight?: IconComponent,
+    IconLeftName?: string,
+    IconRightName?: string,
+    title?: string,
+    OnIconLeftPress?: () => void, // Ação do icone
+    OnIconRightPress?: () => void
+}
 
 export const Input = forwardRef(() => {
     return (
-        <View style={style.boxInput}>
-            <TextInput
-                style={style.input}
-            />
-            <MaterialIcons
-                name="email"
-                size={20}
-                color={themas.colors.gray}
-            />
-        </View>
+        <>
+            <Text style={style.titleInput}>ENDEREÇO DE E-MAIL</Text>
+            <View style={style.boxInput}>
+                <TextInput
+                    style={style.input}
+                />
+                <MaterialIcons
+                    name="email"
+                    size={20}
+                    color={themas.colors.gray}
+                />
+            </View>
+        </>
     )
 })
