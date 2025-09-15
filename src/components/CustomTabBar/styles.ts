791e17ac3ca5c3718d2c6e8b7e97fc0eaeb654e6
Leonardo Pires Tirado <leonardo.tirado@docente.pr.senac.br>
2025-09-15 10:16:57 -0300

    Criando area no bottom, deixando mais organizado para aplicar o botao na segunda tela

--- src/components/CustomTabBar/styles.ts
+++ src/components/CustomTabBar/styles.ts
@@ -2,25 +2,10 @@ import { StyleSheet } from "react-native"
 import { themas } from "../../global/themes"
 
 export const style = StyleSheet.create({
-    button: {
-        width: 250,
-        height: 50,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: themas.colors.primary,
-        borderRadius: 40,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 3,
-        },
-        shadowOpacity: 0.29,
-        shadowRadius: 4.65,
-        elevation: 7,
-    },
-    textButton: {
-        fontSize: 16,
-        color: '#FFF',
-        fontWeight: 'bold'
-    },
+    tabArea: {
+        flexDirection: 'row',
+        height: 80,
+        justifyContent: 'space-around',
+
+    }
 })
