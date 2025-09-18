fb6dc38313f5ca19aa060d2ea87959086a7355cf
Leonardo Pires Tirado <leonardo.tirado@docente.pr.senac.br>
2025-09-18 08:06:52 -0300

    Criando o header da Lista, adicionando estilos, text e style para header

--- src/pages/list/styles.ts
+++ src/pages/list/styles.ts
@@ -1,6 +1,26 @@
 import { StyleSheet, Dimensions } from "react-native";
-import { themas } from "../global/themes";
+import { themas } from "../../global/themes";
 
 export const style = StyleSheet.create({
-    
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        // backgroundColor: 'red'
+    },
+    header: {
+        width: '100%',
+        height: Dimensions.get('window').height/6,
+        backgroundColor: themas.colors.primary,
+        paddingHorizontal: 20,
+        justifyContent: 'center'
+    },
+    greeting: {
+        fontSize: 20,
+        color: '#FFF',
+        marginTop: 20
+    },
+    boxInput: {
+        width: '80%'
+    }
+
 })
