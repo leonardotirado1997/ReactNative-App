0e23ba9f2241556c418284d4dff47113829f2254
Leonardo Pires Tirado <leonardo.tirado@docente.pr.senac.br>
2025-09-22 12:11:05 -0300

    Criando cards e flag, para status dos proprios cards

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -4,6 +4,8 @@ import { style } from "./styles";
 import { Input } from "../../components/input";
 import { MaterialIcons } from '@expo/vector-icons';
 import { Ball } from "../../components/Ball";
+import { Flag } from "../../components/Flag";
+import { themas } from "../../global/themes";
 
 type PropCard = {
     item: number,
@@ -46,7 +48,7 @@ export default function List() {
                             <Text>{item.description}</Text>
                         </View>
                     </View>
-                    {/* <Flag /> */}
+                    <Flag caption="Urgente" color={themas.colors.red} />
                 </View>
             </TouchableOpacity>
         )
