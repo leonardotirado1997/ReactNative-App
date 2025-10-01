44f1eb94f4780e83e8a490da885429e9450d357e
Leonardo Pires Tirado <leonardo.tirado@docente.pr.senac.br>
2025-10-01 08:00:45 -0300

    Arrumando as flags, colocando funcionalidade no JSON para salvar tarefas temporariamente no terminal

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -1,5 +1,8 @@
 import React, { createContext, useContext, useRef, useEffect, useState } from "react";
-import { Dimensions, KeyboardAvoidingView, Platform, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+    Dimensions, KeyboardAvoidingView, Platform, StyleSheet, Text, TouchableOpacity,
+    View
+} from "react-native";
 import { MaterialIcons, AntDesign } from '@expo/vector-icons';
 import { Modalize } from "react-native-modalize";
 import { Input } from "../components/input";
@@ -21,11 +24,12 @@ export const AuthProviderList = (props: any): any => {
     const modalizeRef = useRef<Modalize>(null);
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
-    const [selectedFlag, setSelected] = useState('Urgente');
+    const [selectedFlag, setSelectedFlag] = useState('Urgente');
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [selectedTime, setSelectedTime] = useState(new Date());
     const [showDatePicker, setShowDatePicker] = useState(false);
     const [showTimePicker, setShowTimePicker] = useState(false);
+    const [item, setItem] = useState(0);
 
 
     const onOpen = () => {
@@ -39,11 +43,15 @@ export const AuthProviderList = (props: any): any => {
     const _renderFlags = () => {
         return (
             flags.map((item, index) => (
-                <TouchableOpacity key={index}>
+                <TouchableOpacity key={index}
+                    onPress={() => {
+                        setSelectedFlag(item.caption)
+                    }}
+                >
                     <Flag
                         caption={item.caption}
                         color={item.color}
-                    //selected
+                        selected={item.caption == selectedFlag}
                     />
                 </TouchableOpacity>
             ))
@@ -53,19 +61,27 @@ export const AuthProviderList = (props: any): any => {
         setSelectedDate(date);
     }
     const handleTimeChange = (date) => {
-        setSelected(date);
+        setSelectedTime(date);
     }
 
     const handleSave = () => {
         const newItem = {
-            item: 0,
-            title: 'Titulo',
-            description: 'Descrição',
-            flags: 'Flags',
-            timeLimite: '01.02.2025'
+            item: Date.now(),
+            title,
+            description,
+            flags: selectedFlag,
+            timeLimite: new Date(
+                selectedDate.getFullYear(),
+                selectedDate.getMonth(),
+                selectedDate.getDate(),
+                selectedTime.getHours(),
+                selectedTime.getMinutes()
+            ).toISOString(),
         }
         console.log(newItem)
+
     }
+
 
     const _container = () => {
         return (
