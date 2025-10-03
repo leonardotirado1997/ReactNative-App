7aba0a31bafc20c465af345f64f9da38816f09c5
Leonardo Pires Tirado <leonardo.tirado@docente.pr.senac.br>
2025-10-03 12:24:37 -0300

    Criando tasklist para salvar tarefa, criando constantes para ela e os sets quando salvar a tarefa

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -31,6 +31,7 @@ export const AuthProviderList = (props: any): any => {
     const [showDatePicker, setShowDatePicker] = useState(false);
     const [showTimePicker, setShowTimePicker] = useState(false);
     const [item, setItem] = useState(0);
+    const [taskList, setTaskList] = useState([]);
 
 
     const onOpen = () => {
@@ -40,6 +41,10 @@ export const AuthProviderList = (props: any): any => {
     const onClose = () => {
         modalizeRef?.current?.close();
     }
+
+    useEffect(() => {
+        console.log(taskList.length)
+    }, [taskList]);
 
     const _renderFlags = () => {
         return (
@@ -83,18 +88,30 @@ export const AuthProviderList = (props: any): any => {
                     selectedTime.getMinutes()
                 ).toISOString(),
             }
-            const storageData = await AsyncStorage.getItem('tasklist');
-            console.log(storageData)
+            const storageData = await AsyncStorage.getItem('taskList');
+            //console.log(storageData)
             let taskList = storageData ? JSON.parse(storageData) : [];
+
             taskList.push(newItem);
-            await AsyncStorage.setItem('tasklist', JSON.stringify(taskList))
+            await AsyncStorage.setItem('taskList', JSON.stringify(taskList))
+
+            setTaskList(taskList)
+            setData()
+            onClose()
 
         } catch (error) {
             console.log("Erro ao salvar o item", error)
         }
 
     }
-
+    const setData = () => {
+        setTitle('')
+        setDescription(''),
+        setSelectedFlag('Urgente'),
+        setItem(0)
+        setSelectedDate(new Date())
+        setSelectedTime(new Date())
+    }
 
     const _container = () => {
         return (
@@ -187,7 +204,7 @@ export const AuthProviderList = (props: any): any => {
         )
     }
     return (
-        <AuthContextList.Provider value={{ onOpen }}>
+        <AuthContextList.Provider value={{ onOpen, taskList }}>
             {props.children}
             <Modalize
                 ref={modalizeRef}
