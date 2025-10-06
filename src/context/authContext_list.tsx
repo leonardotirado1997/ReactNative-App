8ca3fc4a253feda8cf338fcfebffe7c67322bc3f
Leonardo Pires Tirado <leonardo.tirado@docente.pr.senac.br>
2025-10-06 12:01:45 -0300

    Renderizando tarefas, aplicando data do formato BR alem de criar novas propriedades para salvar no banco de dados

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -79,8 +79,8 @@ export const AuthProviderList = (props: any): any => {
                 item: Date.now(),
                 title,
                 description,
-                flags: selectedFlag,
-                timeLimite: new Date(
+                flag: selectedFlag,
+                timeLimit: new Date(
                     selectedDate.getFullYear(),
                     selectedDate.getMonth(),
                     selectedDate.getDate(),
--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -7,30 +7,29 @@ import { Ball } from "../../components/Ball";
 import { Flag } from "../../components/Flag";
 import { themas } from "../../global/themes";
 import { AuthContextList } from "../../context/authContext_list";
-
-type PropCard = {
-    item: number,
-    title: string,
-    description: string,
-    flag: 'urgente' | 'opcional'
-}
+import { formatDateToBR } from "../../global/functions";
+import { AuthContextType, PropCard } from "../../global/Props";
 
 export default function List() {
 
     const { taskList } = useContext<AuthContextType>(AuthContextList)
 
     const _renderCard = (item: PropCard) => {
+        const color = item.flag == 'Opcional' ? themas.colors.blueLight : themas.colors.red
         return (
             <TouchableOpacity style={style.card}>
                 <View style={style.rowCard}>
                     <View style={style.rowCardLeft}>
-                        <Ball color="red" />
+                        <Ball color={color} />
                         <View>
                             <Text style={style.titleCard}>{item.title}</Text>
                             <Text style={style.descriptionCard}>{item.description}</Text>
+                            <Text style={style.descriptionCard}>Até {formatDateToBR(item.timeLimit)}</Text>
                         </View>
                     </View>
-                    <Flag caption="Urgente" color={themas.colors.red} />
+                    <Flag 
+                    caption={item.flag} 
+                    color={color} />
                 </View>
             </TouchableOpacity>
         )
