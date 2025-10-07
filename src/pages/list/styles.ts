54401549e4a6a4ae529eeff55a98c22e4d4bb506
Leonardo Pires Tirado <leonardo.tirado@docente.pr.senac.br>
2025-10-07 08:33:36 -0300

    Arrumando partes do card do list, deixando em tamanho responsivo

--- src/pages/list/styles.ts
+++ src/pages/list/styles.ts
@@ -5,7 +5,6 @@ export const style = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        // backgroundColor: 'red'
     },
     header: {
         width: '100%',
@@ -25,11 +24,10 @@ export const style = StyleSheet.create({
     boxList: {
         flex: 1,
         width: '100%',
-        // backgroundColor: 'red'
     },
     card: {
         width: '100%',
-        height: 60,
+        minHeight: 60,
         backgroundColor: '#FFF',
         marginTop: 6,
         borderRadius: 10,
